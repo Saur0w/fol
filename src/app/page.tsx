c7fb857815e427dec538a002e2bd2a46b9ecf793
saur0w <sthapliyal085@gmail.com>
2026-07-02 02:38:50 +0530

    "Images"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,36 @@
 
 import styles from "./page.module.css";
 import Landing from "@/components/Landing";
+import Lenis from "lenis";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useEffect } from "react";
+import { setLenisInstance } from "@/lib/Scoll";
+
+gsap.registerPlugin(ScrollTrigger);
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <Landing />
-    </div>
-  );
+    useEffect(() => {
+        const lenis = new Lenis();
+        setLenisInstance(lenis);
+
+        lenis.on("scroll", ScrollTrigger.update);
+
+        function raf(time: number) {
+            lenis.raf(time);
+            requestAnimationFrame(raf);
+        }
+        requestAnimationFrame(raf);
+
+        return () => {
+            lenis.destroy();
+            setLenisInstance(null);
+        };
+    }, []);
+
+    return (
+        <div className={styles.page}>
+            <Landing />
+        </div>
+    );
 }
---
+++ src/lib/Scoll/index.tsx
@@ -0,0 +1,12 @@
+"use client";
+import Lenis from "lenis";
+
+let lenisInstance: Lenis | null = null;
+
+export function setLenisInstance(instance: Lenis | null) {
+    lenisInstance = instance;
+}
+
+export function getLenisInstance() {
+    return lenisInstance;
+}
