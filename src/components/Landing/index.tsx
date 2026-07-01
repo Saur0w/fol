1c00d7bb20d96137a6bec456650f585f5ee929ff
saur0w <sthapliyal085@gmail.com>
2026-07-01 21:45:36 +0530

    "Landing"

---
+++ src/components/Landing/index.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import styles from "./style.module.scss";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { SplitText } from "gsap/SplitText";
+import { useRef } from "react";
+
+gsap.registerPlugin(useGSAP, ScrollTrigger, SplitText);
+
+export default function Landing() {
+    return (
+        <section className={styles.landing}>
+
+        </section>
+    )
+}
